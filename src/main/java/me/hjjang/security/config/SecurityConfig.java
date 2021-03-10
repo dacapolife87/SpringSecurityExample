d87615264b76508ca42ac3d85503c92fb380862e
HyungjuJang <dacapolife87@gmail.com>
2021-03-10 12:16:30 +0900

    feat : logout example

--- src/main/java/me/hjjang/security/config/SecurityConfig.java
+++ src/main/java/me/hjjang/security/config/SecurityConfig.java
@@ -8,10 +8,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Configuration
@@ -24,27 +27,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .authorizeRequests()
             .anyRequest().authenticated();
         http
-            .formLogin()
-//        .loginPage("/loginPage")
-        .defaultSuccessUrl("/")
-        .failureUrl("/login")
-        .usernameParameter("userId")
-        .passwordParameter("passwd")
-        .loginProcessingUrl("/login_proc")
-        .successHandler(new AuthenticationSuccessHandler() {
-            @Override
-            public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
-                System.out.println("authentication"+ authentication.getName());
-                httpServletResponse.sendRedirect("/");
-            }
-        })
-        .failureHandler(new AuthenticationFailureHandler() {
-            @Override
-            public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
-                System.out.println("exception" + e.getMessage());
-                httpServletResponse.sendRedirect("/");
-            }
-        })
-        .permitAll();
+            .formLogin();
+        http.logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .addLogoutHandler(new LogoutHandler() {
+                    @Override
+                    public void logout(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) {
+                        HttpSession session = httpServletRequest.getSession();
+                        session.invalidate();
+                    }
+                })
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+                        httpServletResponse.sendRedirect("/login");
+                    }
+                })
+                .deleteCookies("remember-me");
     }
 }
